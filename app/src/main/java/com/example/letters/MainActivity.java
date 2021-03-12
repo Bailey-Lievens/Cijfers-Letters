a995f2fa6e97df3d95256d97337d4b68ab614bb7
Bailey Lievens <baileylievens@hotmail.be>
2021-03-12 16:05:39 +0100

    final version :) also initial lmao

---
+++ app/src/main/java/com/example/letters/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.letters;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    Fragment currentLayout; //holds current fragment that's being shown
+
+    final LetterFragment letterFragment = new LetterFragment();
+    final NumberFragment numberFragment = new NumberFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        currentLayout = letterFragment; //Starting Layout
+
+        Button btnChange = findViewById(R.id.btn_switch); //switch button to change
+
+        FragmentManager fm = getSupportFragmentManager(); //initialize fragment manager
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragmentSpot, currentLayout); //replace starting fragment with LetterFragment
+        ft.commit();
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (currentLayout == letterFragment) {
+                    currentLayout = numberFragment;
+                } else {
+                    currentLayout = letterFragment;
+                }
+
+                FragmentManager fm = getSupportFragmentManager();
+                FragmentTransaction ft = fm.beginTransaction();
+                ft.replace(R.id.fragmentSpot, currentLayout);
+                ft.commit();
+            }
+        });
+    }
+}
